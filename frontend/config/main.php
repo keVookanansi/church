1269075395786bacd06a08f2994025da2d876b28
keVookanansi <kelvinkanansi@gmail.com>
2025-06-21 19:18:47 +0300

    Update main.php

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -1,10 +1,5 @@
 <?php
-$params = array_merge(
-    require __DIR__ . '/../../common/config/params.php',
-    require __DIR__ . '/../../common/config/params-local.php',
-    require __DIR__ . '/params.php',
-    require __DIR__ . '/params-local.php'
-);
+$params = [];
 
 return [
     'id' => 'app-frontend',
