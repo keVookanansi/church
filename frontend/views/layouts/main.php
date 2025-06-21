217e6de3e5e2eb8f1387204876e891d39639f8aa
keVookanansi <kelvinkanansi@gmail.com>
2025-06-21 20:03:10 +0300

    Update main.php

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -9,8 +9,10 @@ use yii\helpers\Url;
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
 <head>
-    <meta charset="<?= Yii::$app->charset ?>">
+   <meta charset="<?= Yii::$app->charset ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
     <!-- Bootstrap CSS -->
