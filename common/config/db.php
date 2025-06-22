774a814f46201066efd03797f5305c7de8bb9520
keVookanansi <kelvinkanansi@gmail.com>
2025-06-22 13:51:42 +0300

    Update db.php

--- common/config/db.php
+++ common/config/db.php
@@ -10,7 +10,7 @@ $parsed = parse_url($url);
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'pgsql:host=' . $parsed['host'] . ';port=' . ($parsed['port'] ?? 5432) . ';dbname=' . ltrim($parsed['path'], '/'),
+    'dsn' => 'pgsql:host=' . $parsed['dpg-d1b7dtuuk2gs739ei8sg-a'] . ';port=' . ($parsed['port'] ?? 5432) . ';dbname=' . ltrim($parsed['path'], '/'),
     'username' => $parsed['user'],
     'password' => $parsed['pass'],
     'charset' => 'utf8',
